6bf6d0ba36177d7e0368a63be5911f0c91ff1bb9
motemen <motemen@gmail.com>
2011-04-01 12:20:48 +0900

    initial commit

---
+++ lib/DBIx/MoCo/Lite.pm
@@ -0,0 +1,34 @@
+package DBIx::MoCo::Lite;
+use strict;
+use warnings;
+
+our $VERSION = '0.01';
+
+1;
+
+__END__
+
+=head1 NAME
+
+DBIx::MoCo::Lite -
+
+=head1 SYNOPSIS
+
+  use DBIx::MoCo::Lite;
+
+=head1 DESCRIPTION
+
+DBIx::MoCo::Lite is
+
+=head1 AUTHOR
+
+motemen E<lt>motemen@gmail.comE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
